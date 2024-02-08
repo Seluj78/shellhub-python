91c5485d4c1cc4d436d9ef5749983f03c744df4d
Jules Lasne <jules.lasne@gmail.com>
2024-02-08 18:47:00 +0100

    Core: Fixed models import

--- shellhub/__init__.py
+++ shellhub/__init__.py
@@ -1,5 +1,5 @@
 # Increment versions here according to SemVer
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .models.device import ShellHubDevice, ShellHubDeviceInfo
 from .models.base import ShellHub
